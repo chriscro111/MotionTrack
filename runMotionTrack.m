64d3c1abe51dbedf9b772468be9bf8c926a06fa4
Chris Cronin <chriscro111@gmail.com>
2015-11-14 23:27:55 -0500

    Inital Commit - outline basic framework

---
+++ runMotionTrack.m
@@ -0,0 +1,10 @@
+% load video file and separate into frames
+
+% process each frame into a binary image with the balls in the foreground
+
+% segment image with labels for each group of pixels corresponding to a
+% ball
+
+% calculate the centers of each group of pixels
+
+% some kind of plot/animation showing position over time for each ball??

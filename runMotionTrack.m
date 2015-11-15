d76b979b23c5fa54130a236fb90150f866f907f9
alfres <gandrew2@vt.edu>
2015-11-15 00:30:59 -0500

    Loads a video file and splits it into a 4-D array of frames

--- runMotionTrack.m
+++ runMotionTrack.m
@@ -1,5 +1,7 @@
 % load video file and separate into frames
-
+path = 'testMedia/53601da914442f9b28000001-b30-600.mp4';
+v = VideoReader(path);
+frames = read(v,[1,Inf]);
 % process each frame into a binary image with the balls in the foreground
 
 % segment image with labels for each group of pixels corresponding to a

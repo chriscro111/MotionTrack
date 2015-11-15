d2b2cc2faee490e6fb836950661c5544a9a19844
alfres <gandrew2@vt.edu>
2015-11-15 09:37:42 -0500

    Testing for converting frame to binary image

--- runMotionTrack.m
+++ runMotionTrack.m
@@ -3,7 +3,14 @@ path = 'testMedia/53601da914442f9b28000001-b30-600.mp4';
 v = VideoReader(path);
 frames = read(v,[1,Inf]);
 % process each frame into a binary image with the balls in the foreground
+%% Testing processing frames into binary images
+im = imread('testMedia/yelloBallTest.jpg');
+labIm = rgb2lab(im);
+bin = zeros(size(labIm,1),size(labIm,2));
+bin = labIm(:,:,3) >= 30 & labIm(:,:,3) <= 70;
+imshow(bin);
 
+    
 % segment image with labels for each group of pixels corresponding to a
 % ball
 

98b047da3c85f6d4c4ba7ecf7b5334dedc71b272
Chris Cronin <chriscro111@gmail.com>
2015-11-15 19:11:19 -0500

    fixed center finding algorith, added 3 ball test file

--- runMotionTrack.m
+++ runMotionTrack.m
@@ -1,14 +1,14 @@
 % load video file and separate into frames
-path = 'testMedia/lowres_2balls.mp4';
+path = 'testMedia/lowres_3balls.mp4';
 % path = 'testMedia/lowres_test.mp4';
 v = VideoReader(path);
-frames = read(v,[1,Inf]);
+frames = read(v,[86,110]);
 [rows, cols, ~, numFrames] = size(frames);
 
 %% Testing processing frames into binary images
 %im = imread('testMedia/yelloBallTest.jpg');
 % manually select threshold
-labIm = rgb2lab(frames(:,:,:,167));
+labIm = rgb2lab(frames(:,:,:,1));
 thresh = threshTool(labIm(:,:,3));
 for i=1:numFrames
     labIm = rgb2lab(frames(:,:,:,i));
@@ -30,125 +30,45 @@ end
 %% segment image with labels for each group of pixels corresponding to a
 % ball
 labels = zeros(rows, cols, 1, numFrames);
-numBlobs = zeros(numFrames, 1);
+blobCount = zeros(numFrames, 1);
 for i = 1:numFrames
-    [labels(:, :, i), numBlobs(i)] = bwlabel(bin(:, :, i));
+    [labels(:, :, i), blobCount(i)] = bwlabel(bin(:, :, i));
 end
 
 %% calculate the centers of each group of pixels
-numBlobs = mode(numBlobs);
+numBlobs = mode(blobCount);
 centers = zeros(numFrames, 2, numBlobs);
-dist1 = Inf;
-dist2 = Inf;
-dist3 = Inf;
-for i=1:size(labels,4)
-   blob1X = 0;
-   blob1Y = 0;
-   blob2X = 0;
-   blob2Y = 0;
-   blob3X = 0;
-   blob3Y = 0;
-   blob1Count = 0;
-   blob2Count = 0;
-   blob3Count = 0;
-   for j=1:size(labels,1)
-      for k=1:size(labels,2)
-          if labels(j,k,1,i) > 0
-              switch labels(j,k,1,i)
-                  case 1
-                      blob1X = blob1X + k;
-                      blob1Y = blob1Y + j;
-                      blob1Count = blob1Count + 1;
-                  case 2
-                      blob2X = blob2X + k;
-                      blob2Y = blob2Y + j;
-                      blob2Count = blob2Count + 1;
-                  case 3
-                      blob3X = blob3X + k;
-                      blob3Y = blob3Y + j;
-                      blob3Count = blob3Count + 1;
-              end
-          end
-      end
-   end
-   if blob1Count > 0
-    avgX = blob1X / blob1Count;
-    avgY = blob1Y / blob1Count;
-    if i > 1
-        dist1 = abs(sqrt((avgX^2-centers(i-1,1,1)^2)+(avgY^2-centers(i-1,2,1)^2)));
-        if(numBlobs > 1)
-            dist2 = abs(sqrt((avgX^2-centers(i-1,1,2)^2)+(avgY^2-centers(i-1,2,2)^2)));
-        end
-        if(numBlobs > 2)
-            dist3 = abs(sqrt((avgX^2-centers(i-1,1,3)^2)+(avgY^2-centers(i-1,2,3)^2)));
-        end
-        if dist1 < dist2 && dist1 < dist3
-            centers(i,1,1) = avgX;
-            centers(i,2,1) = avgY;
-        elseif dist2 < dist3 
-            centers(i,1,2) = avgX;
-            centers(i,2,2) = avgY;
-        else
-            centers(i,1,3) = avgX;
-            centers(i,2,3) = avgY;
-        end
-    else
-       centers(i,1,1) = avgX;
-       centers(i,2,1) = avgY; 
+dist = zeros(numBlobs);
+coords = zeros(numBlobs, 2);
+% compute first center
+for b = 1:numBlobs
+    [y, x] = find(labels(:,:,b) == b);
+    x = sum(x) / length(x);
+    y = sum(y) / length(y);
+    centers(1, 1, b) = x;
+    centers(1, 2, b) = y;
+end
+for i = 2:numFrames
+    % compute centers
+    for b = 1:numBlobs
+        [y, x] = find(labels(:,:,i) == b);
+        x = sum(x) / length(x);
+        y = sum(y) / length(y);
+        coords(b, 1) = x;
+        coords(b, 2) = y;
     end
-   end
-   if blob2Count > 0
-    avgX = blob2X / blob2Count;
-    avgY = blob2Y / blob2Count;
-    if i > 1
-        dist1 = abs(sqrt((avgX^2-centers(i-1,1,1)^2)+(avgY^2-centers(i-1,2,1)^2)));
-        if(numBlobs > 1)
-            dist2 = abs(sqrt((avgX^2-centers(i-1,1,2)^2)+(avgY^2-centers(i-1,2,2)^2)));
-        end
-        if(numBlobs > 2)
-            dist3 = abs(sqrt((avgX^2-centers(i-1,1,3)^2)+(avgY^2-centers(i-1,2,3)^2)));
-        end
-        if dist1 < dist2 && dist1 < dist3
-            centers(i,1,1) = avgX;
-            centers(i,2,1) = avgY;
-        elseif dist2 < dist3 
-            centers(i,1,2) = avgX;
-            centers(i,2,2) = avgY;
-        else
-            centers(i,1,3) = avgX;
-            centers(i,2,3) = avgY;
-        end
-    else
-       centers(i,1,2) = avgX;
-       centers(i,2,2) = avgY; 
+    % compute distances
+    for b = 1:numBlobs
+    for b2 = 1:numBlobs
+        dist(b, b2) = (centers(i-1, 1, b) - coords(b2, 1))^2 + (centers(i-1, 2, b) - coords(b2, 2))^2;
     end
-   end
-   if blob3Count > 0
-    avgX = blob3X / blob3Count;
-    avgY = blob3Y / blob3Count;
-    if i > 1
-        dist1 = abs(sqrt((avgX^2-centers(i-1,1,1)^2)+(avgY^2-centers(i-1,2,1)^2)));
-        if(numBlobs > 1)
-            dist2 = abs(sqrt((avgX^2-centers(i-1,1,2)^2)+(avgY^2-centers(i-1,2,2)^2)));
-        end
-        if(numBlobs > 2)
-            dist3 = abs(sqrt((avgX^2-centers(i-1,1,3)^2)+(avgY^2-centers(i-1,2,3)^2)));
-        end
-        if dist1 < dist2 && dist1 < dist3
-            centers(i,1,1) = avgX;
-            centers(i,2,1) = avgY;
-        elseif dist2 < dist3 
-            centers(i,1,2) = avgX;
-            centers(i,2,2) = avgY;
-        else
-            centers(i,1,3) = avgX;
-            centers(i,2,3) = avgY;
-        end
-    else
-       centers(i,1,3) = avgX;
-       centers(i,2,3) = avgY; 
     end
-   end
+    % compute matching centers
+    for b = 1:numBlobs
+        [~, ind] = min(dist(b, :), [], 2);
+        centers(i, 1, b) = coords(ind, 1);
+        centers(i, 2, b) = coords(ind, 2);
+    end
 end
 
 % %% display center
